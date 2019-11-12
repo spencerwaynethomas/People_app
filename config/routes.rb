2d17a64cb32c80e3b74fa666b1239fade97f472f
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2019-11-12 15:02:19 -0700

    "people app push"

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'people/index'
+  get 'people/show'
+  get 'people/edit'
+  get 'people/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
