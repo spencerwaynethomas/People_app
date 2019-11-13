ff9322d9b07604ee0ab9207852f02c39c2b59870
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2019-11-12 17:14:12 -0700

    person app finish

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,13 +1,50 @@
 class PeopleController < ApplicationController
   def index
+    @people = Person.all
   end
 
   def show
+    @people = Person.find(params[:id])
   end
 
   def edit
+    @person = Person.find(params[:id])
   end
 
   def new
+    @person = Person.new
   end
-end
+  
+  def update
+    @person = Person.find(params[:id])
+    if @person.update(person_params)
+      redirect_to people_path
+    else
+      render :edit
+    end
+  end
+  def create
+    @person = Person.new(people_params)
+    if @person.save
+      redirect_to people_path
+      else
+        render :new
+      end
+    end
+  end
+
+  def destroy
+    Person.find(params[:id]).destroy
+    redirect_to people_path
+  end 
+  
+  def person_params
+    params.require(:person).permit(:first_name, :last_name)
+  end
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'people/index'
-  get 'people/show'
-  get 'people/edit'
-  get 'people/new'
+ resources :people
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
